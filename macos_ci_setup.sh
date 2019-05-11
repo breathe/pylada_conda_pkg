1c250bd21145328e81a5960cc625db45270ef151
N. Ben Cohen <breathevalue@icloud.com>
2019-05-11 16:00:35 -0600

    Update CI to prepare macos 10.9 sdk in /opt/ when building on osx

--- macos_ci_setup.sh
+++ macos_ci_setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -x
 
 # needed for some tests
-brew install tree
+brew install tree wget
 
 # install MacOS 10.9 sdk to /opt/MacOSX10.9.sdk
 (
     sudo mkdir -p /opt
     cd /opt
-    curl -O https://github.com/phracker/MacOSX-SDKs/releases/download/10.13/MacOSX10.9.sdk.tar.xz
-    tar -zxvf MacOSX10.9.sdk.tar.xz
+    wget https://github.com/phracker/MacOSX-SDKs/releases/download/10.13/MacOSX10.9.sdk.tar.xz
+    tar -xvf MacOSX10.9.sdk.tar.xz
     rm MacOSX10.9.sdk.tar.xz
 )
 
